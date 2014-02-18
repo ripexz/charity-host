95714bf20c3e1b1df8cd60048060f62d287d3339
ripexz <ripexz@gmail.com>
2014-02-18 17:27:30 +0000

    Login barebones

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<?php
+
+	session_start();
+	require_once('../core/lib/output.php');
+	require_once('../core/lib/db.php');
+
+	if ( isset($_SESSION['authorised']) ) {
+		header("Location: home.php");
+	}
+
+	output_header("Login");
+
+	echo '<div class="container">
+			<form class="form-signin" role="form">
+				<h2 class="form-signin-heading">Please sign in</h2>
+				<input type="email" class="form-control" placeholder="Email address" required autofocus>
+				<input type="password" class="form-control" placeholder="Password" required>
+				<label class="checkbox">
+					<input type="checkbox" value="remember-me"> Remember me
+				</label>
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+			</form>
+		</div>';
+
+	output_footer();
+
+?>
--- core/lib/output.php
+++ core/lib/output.php
@@ -6,14 +6,14 @@
 				<head>
 					<meta charset=\"utf-8\" />
 					<title>{$title} | Charity Host</title>
-					<script src=\"core/js/knockout-3.0.0.js\"></script>
+					<script src=\"/core/js/knockout-3.0.0.js\"></script>
 					<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>
-					<script src=\"core/js/bootstrap.min.js\"></script>
-					<script src=\"core/js/main.js\"></script>
+					<script src=\"/core/js/bootstrap.min.js\"></script>
+					<script src=\"/core/js/main.js\"></script>
 					<link rel=\"stylesheet\" href=\"http://fonts.googleapis.com/css?family=Open+Sans:400,700,600\">
-					<link rel=\"stylesheet\" href=\"core/css/bootstrap.min.css\" />
-					<link rel=\"stylesheet\" href=\"core/css/bootstrap-theme.min.css\" />
-					<link rel=\"stylesheet\" href=\"core/css/main.css\" />
+					<link rel=\"stylesheet\" href=\"/core/css/bootstrap.min.css\" />
+					<link rel=\"stylesheet\" href=\"/core/css/bootstrap-theme.min.css\" />
+					<link rel=\"stylesheet\" href=\"/core/css/main.css\" />
 				</head>
 			 <body>
 				<div id=\"wrapper\"> <!-- start of wrapper -->";
