236f9f18821ed68a147577ae1f8e0f1553de82ee
ripexz <ripexz@gmail.com>
2014-02-24 14:35:26 +0000

    Charity base

---
+++ core/lib/charity.php
@@ -0,0 +1,82 @@
+<?
+	function output_header($charity, $title = "Home") {
+		echo "<!DOCTYPE html>
+			<html lang=\"en\">
+				<head>
+					<meta charset=\"utf-8\" />
+					<title>{$title} | {$charity} | Charity Host</title>
+					<script src=\"/core/js/knockout-3.0.0.js\"></script>
+					<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>
+					<script src=\"/core/js/bootstrap.min.js\"></script>
+					<script src=\"/core/js/main.js\"></script>
+					<link rel=\"stylesheet\" href=\"http://fonts.googleapis.com/css?family=Open+Sans:400,700,600\">
+					<link rel=\"stylesheet\" href=\"/core/css/bootstrap.min.css\" />
+					<link rel=\"stylesheet\" href=\"/core/css/bootstrap-theme.min.css\" />
+					<link rel=\"stylesheet\" href=\"/core/css/charity.css\" />
+				</head>
+			 <body>
+				<div id=\"wrapper\"> <!-- start of wrapper -->";
+
+		echo "<header>";
+
+		echo '<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">{$charity}</a>
+					</div>
+					<div class="collapse navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li><a href="#contact">Contact</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>';
+
+		echo '<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<div class="navbar-collapse collapse" style="height: 1px;">
+						<ul class="nav navbar-nav">
+							<li class="active"><a class="ajax" href="home">Home</a></li>
+							<li><a class="ajax" href="faq">FAQ</a></li>
+							<li><a href="admin/register.php">Register</a></li>
+							<li><a href="admin">Login</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>';
+
+		echo '<div class="container">
+				<h1>{$title}</h1>
+			</div>';
+		echo "</header>";
+	}
+
+	function output_footer($charity, $org = "Charity Host") {
+		echo "<footer>
+				<div class=\"container\">
+					<p class=\"pull-right\"><a href=\"#\">Back to top</a></p>
+					<p>&copy;" . date('Y') . " " . $charity . ". Powered by " . $org . "</p>
+				</div>
+			</footer>
+			</div> <!-- end of wrapper -->
+			</body>
+			</html>";
+	}
+?>
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+	require_once("lib/charity.php");
+
+	output_header("Test Charity");
+
+	echo "<div class=\"container\">";
+
+	echo "Charity homepage content goes here";
+
+	echo "</div>";
+
+	output_footer("Test Charity");
+
+?>
