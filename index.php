b65495776ad0a94f3773eefb9ce264d34391b4c3
ripexz <ripexz@gmail.com>
2014-02-24 15:28:06 +0000

    Redirection tweaks

--- index.php
+++ index.php
@@ -3,32 +3,49 @@
 	require_once('core/lib/db.php');
 	require_once('core/lib/output.php');
 
-	$page = "home";
-	$title = "Charity Host";
-	if (isset($_GET['page'])) {
-		$page = htmlentities($_GET['page']);
-		if ($page == "faq") {
-			$title = "FAQ";
-		}
+	$req = "home";
+	if (isset($_GET["req"])) {
+		//prepare string:
+		$req = trim((string) $_GET["req"]);
+
+		//remove slashes from start or end:
+		$req = trim($req, '/');
 	}
 
-	output_header($title);
+	$reqArr = explode('/', $req);
+	if (isset($reqArr[1])) {
+		//charity stuff
+		header("Location: test.php", true);
+	}
+	else {
+		switch ($reqArr[0]) {
+			case 'faq':
+				$page = "faq";
+				$title = "FAQ";
+				break;
+			
+			default:
+				$page = "home";
+				$title = "Home";
+				break;
+		}
 
-	echo "<div class=\"container\">";
+		output_header($title);
+		echo "<div class=\"container\">";
 
-	echo "<div id=\"content\">";
-	echo "<script type=\"text/javascript\">
-			$(document).ready(function(){
-				mvvm.getContent('{$page}', '{$title}');
-				$('.navbar li.active').removeClass('active');
-				$('.navbar a[href={$page}]').parent().addClass('active');
-			});
-		</script>";
-	echo "</div>";
+		echo "<div id=\"content\">";
+		echo "<script type=\"text/javascript\">
+				$(document).ready(function(){
+					mvvm.getContent('{$page}', '{$title}');
+					$('.navbar li.active').removeClass('active');
+					$('.navbar a[href={$page}]').parent().addClass('active');
+				});
+			</script>";
+		echo "</div>";
 
-	echo "</div>";
-
-	output_footer();
+		echo "</div>";
+		output_footer();
+	}
 
 	/*
 	$db = new db(null);
