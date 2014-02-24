b5fed2f2520014b1682af6d3161ae0015cfbb782
ripexz <ripexz@gmail.com>
2014-02-24 13:58:09 +0000

    Dashboard tweaks

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -36,21 +36,19 @@
 				<div class="col-sm-3 col-md-2 sidebar">
 					<ul class="nav nav-sidebar">
 						<li class="active"><a href="#">Overview</a></li>
-						<li><a href="#">Reports</a></li>
-						<li><a href="#">Analytics</a></li>
-						<li><a href="#">Export</a></li>
+						<li><a href="#">Pages</a></li>
+						<li><a href="#">Lost & Found</a></li>
+						<li><a href="#">Sponsored Animals</a></li>
+						<li><a href="#">Gallery</a></li>
 					</ul>
 					<ul class="nav nav-sidebar">
-						<li><a href="">Nav item</a></li>
-						<li><a href="">Nav item again</a></li>
-						<li><a href="">One more nav</a></li>
-						<li><a href="">Another nav item</a></li>
-						<li><a href="">More navigation</a></li>
+						<li><a href="#">Settings</a></li>
+						<li><a href="#">Users</a></li>
+						<li><a href="#">Donations</a></li>
 					</ul>
 					<ul class="nav nav-sidebar">
-						<li><a href="">Nav item again</a></li>
-						<li><a href="">One more nav</a></li>
-						<li><a href="">Another nav item</a></li>
+						<li><a href="#">Your profile</a></li>
+						<li><a href="#">Log out</a></li>
 					</ul>
 				</div>
 				<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
