43ffcd4d16460e801edf16644dc403731f3ab4fb
ripexz <ripexz@gmail.com>
2014-03-06 14:52:37 +0000

    Added upload handling

--- admin/gallery.php
+++ admin/gallery.php
@@ -7,14 +7,57 @@
 	require_once('../core/lib/admin.php');
 	require_once('../core/lib/db.php');
 
+	$db = new db(null);
+	$conn = $db->connect();
+	$charity_id = (int) $_SESSION["charity_id"];
+
 	output_admin_header("Gallery", $_SESSION["charity_name"], "admin");
 	echo '<button class="btn btn-lg btn-primary btn-top-right" data-backdrop="static" data-keyboard="false" data-toggle="modal" data-target="#uploadModal">Upload</button>';
 	echo '<div id="imagesView">';
 
-	$db = new db(null);
-	$conn = $db->connect();
-	$charity_id = (int) $_SESSION["charity_id"];
-	$result = $conn->query("SELECT i.* FROM images i JOIN charity_images ci ON ci.image_id = i.id WHERE ci.charity_id = {$charity_id} ORDER BY i.id DESC");
+	if (isset($_POST["submit"])) {
+		$errors = array();
+		$title = get_required_string($_POST, "imagetitle", "Image title", 255, $errors);
+
+		if ($_FILES["imagefile"]["error"] !== UPLOAD_ERR_OK) {
+			$errors[] = "Image upload failed.";
+		}
+
+		if ($_FILES["imagefile"]["size"] > 2097152) {
+			$errors[] = "Image file is too big, please select an image that is under 2 MB.";
+		}
+
+		if (count($errors) == 0) {
+			$info = getimagesize($_FILES["file"]["tmp_name"]);
+			if ($info === FALSE) {
+				$errors[] = "Unable to determine image type of uploaded file.";
+			}
+			else {
+				$title = $conn->real_escape_string($title);
+				$filename = $charity_id . "_" . mt_rand(10, 99) . "_" . time();
+				$upload_to = "/core/uploads/" . $filename;
+
+				if (move_uploaded_file($_FILES["imagefile"]["tmp_name"], $upload_to)) {
+					//Add db entry to images:
+					$safe_url = $conn->real_escape_string($filename);
+					$result = $conn->query("INSERT INTO images (title, url) VALUES ('{$title}', '{$filename}')");
+					if ($result) {
+						$image_id = $conn->insert_id;
+
+						//Add db entry to charity_images:
+						$result2 = $conn->query("INSERT INTO charity_images (image_id, charity_id) VALUES ({$image_id}, {$charity_id})");
+					}
+				}
+				else {
+					$errors[] = "Image upload failed.";
+				}
+			}
+		}
+
+		foreach ($errors as $error) {
+			echo "<div class=\"alert alert-danger\"><strong>Error: </strong>{$error}</div>";
+		}
+	}
 
 	echo '<div><input data-bind="value: searchText, valueUpdate: \'afterkeydown\'" type="text" class="form-control" placeholder="Search images by title"></div>';
 	echo '<div id="image-list">';
@@ -22,7 +65,7 @@
 	echo '<div data-bind="foreach: visibleImages">
 			<div><img data-bind="attr: {src: url, title: title, alt: title}"/></div>
 		</div>';
-	
+
 	echo '</div>';
 
 	// Upload modal:
@@ -37,11 +80,12 @@
 					<div class="modal-body">
 						<div class="form-group">
 							<label for="title">Image title</label>
-							<input type="text" class="form-control" id="title" placeholder="Image title">
+							<input name="imagetitle" type="text" class="form-control" id="title" placeholder="Image title" required>
 						</div>
 						<div class="form-group">
-							<label for="image">File input</label>
-							<input type="file" id="image">
+							<label for="image">Choose an image</label>
+							<input name="imagefile" type="file" id="image" required>
+							<p class=\"help-block\">No larger than 2MB in size.</p>
 						</div>
 					</div>
 					<div class="modal-footer">
