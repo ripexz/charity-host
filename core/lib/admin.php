0b4130aed164c3d31b19bc4ce73ec77274b4bf4c
ripexz <ripexz@gmail.com>
2014-03-13 17:28:07 +0000

    Adjusted admin faq link

--- core/lib/admin.php
+++ core/lib/admin.php
@@ -44,7 +44,7 @@
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="dashboard.php">Dashboard</a></li>
 						<li><a href="settings.php">Settings</a></li>
-						<li><a target="_blank" href="http://www.charityhost.eu/faq">FAQ</a></li>
+						<li><a href="faq.php">FAQ</a></li>
 						<li><a href="logout.php">Log out</a></li>
 					</ul>
 				</div>
