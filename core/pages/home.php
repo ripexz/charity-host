7d1d8c73d9a33ff7a32a76bfbc55052baa6a8568
ripexz <ripexz@gmail.com>
2014-03-15 13:53:00 +0000

    Home page content

--- core/pages/home.php
+++ core/pages/home.php
@@ -1,15 +1,25 @@
 <div>
-	<h1>Features</h1>
-	<h2>Feature 1</h2>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<h2>Feature 2</h2>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<h2>Feature 3</h2>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<h1>Welcome to CharityHost.eu</h1>
+	<p>We provide a free of charge service to Non-Profit & Charity organisations for creating and maintaining an online presence.</p>
+	<p>Just like businesses, it is crucial for Non-Profits to develop and maintain a true internet presence. A great website can provide a charity with many great benefits such as publicity, public awareness, recruiting volunteers, finding lost animals, and most importantly receiving donations.</p>
+	<p>With CharityHost.eu, creating your charity's website is a free and simple process, and we’re here to help you every step along the way.</p>
+	<p>Register or Login to start creating your own charity website.</p>
+	<p>Please check out our video tutorial for guidance, or look at the frequently asked questions section for further help.</p>
+	
+	<div class="feature">
+		<h2>Lost and Found</h2>
+		<p>The Lost and Found feature we provide allows the public to post on your charities lost and found page about a lost animal missing in the area. This can then be removed from the site by you or the person who posted it once the animal has been found.</p>
+	</div>
+	<div class="feature">
+		<h2>Sponsor an Animal</h2>
+		<p>This feature allows you to post ads for animals looking for sponsorship or adoption. Donations can be made to specific animals, which will appear in your account information.</p>
+	</div>
+	<div class="feature">
+		<h2>Donations</h2>
+		<p>The donations feature of our website allows your charity to accept donations online through your website. Donations are paid through PayPal, and we do not take a percentage of any donations you receive.</p>
+	</div>
+	<div class="feature">
+		<h2>Custom Web Pages</h2>
+		<p>This feature allows you to create and customise webpages for your site. You can change the layout and content as you wish.</p>
+	</div>
 </div>
