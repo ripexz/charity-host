ef8e5ac58a3b8c96e3ddcdfb4e17796a496380ed
ripexz <ripexz@gmail.com>
2014-03-15 19:54:02 +0000

    More donate work

--- donate.php
+++ donate.php
@@ -2,6 +2,29 @@
 
 	require_once('core/lib/db.php');
 
-	//todo get donation data
+	//check params
+	if (isset($_GET['charity_id'])) {
+		$charity_id = (int) $_GET['charity_id'];
+
+		if ($charity_id > 0) {
+
+			$db = new db(null);
+			$conn = $db->connect();
+
+			if (isset($_GET['animal_id'])) {
+				// Sponsoring an animal
+				$animal_id = (int) $_GET['animal_id'];
+				$query = "";
+			}
+			else {
+				// General donations
+				$query = "";
+			}
+			$result = $conn->query($query);
+		}
+	}
+
+	// Return to source on error:
+	header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 ?>
