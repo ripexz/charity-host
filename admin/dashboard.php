661dfd65b9a8b07feb54af6ff12915068ab07a81
ripexz <ripexz@gmail.com>
2014-03-17 20:23:22 +0000

    Minor fix

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -13,12 +13,13 @@
 			<p>Welcome to your control panel.</p>
 			<p>This is where you can create and edit your charity website. There are several options to the side.</p>
 			<ul>
-				<li><strong>Pages:</strong> update and create new pages for your site.</li>
-				<li><strong>Lost & Found:</strong>  manage your lost and found adverts.</li>
-				<li><strong>Sponsored Animals:</strong> manage your sponsored animals.</li>
+				<li><strong>Pages:</strong> Update and create new pages for your site.</li>
+				<li><strong>Lost & Found:</strong> Manage your lost and found adverts.</li>
+				<li><strong>Sponsored Animals:</strong> Manage your sponsored animals.</li>
+				<li><strong>Donations:</strong> View donation history.</li>
 				<li><strong>Gallery:</strong> Upload or delete your images here.</li>
-				<li><strong>Settings:</strong> change your account information.</li>
-				<li><strong>Users:</strong> add and remove admins for your site</li>
+				<li><strong>Settings:</strong> Change your account information.</li>
+				<li><strong>Users:</strong> Add and remove admins for your site</li>
 			</ul>
 			<p>Watch our video tutorial or refer to the FAQ section of our site if you need any help.</p>
 		</div>';
