1fbba864bb529604a2088ec1d6d40e75aea9d644
ripexz <ripexz@gmail.com>
2014-03-17 20:44:28 +0000

    Front page tweaks

--- core/pages/home.php
+++ core/pages/home.php
@@ -6,28 +6,28 @@
 	<p>Register or Login to start creating your own charity website.</p>
 	<p>Please check out our video tutorial for guidance, or look at the frequently asked questions section for further help.</p>
 	
-	<div class="feature">
+	<div class="feature row">
 		<h2>Lost and Found</h2>
 		<div class="feature-image">
 			<img src="http://www.placekitten.com/300/149" alt="Image" width="300" height="149" />
 		</div>
 		<p>The Lost and Found feature we provide allows the public to post on your charities lost and found page about a lost animal missing in the area. This can then be removed from the site by you or the person who posted it once the animal has been found.</p>
 	</div>
-	<div class="feature">
+	<div class="feature row">
 		<h2>Sponsor an Animal</h2>
 		<div class="feature-image">
 			<img src="http://www.placekitten.com/300/149" alt="Image" width="300" height="149" />
 		</div>
 		<p>This feature allows you to post ads for animals looking for sponsorship or adoption. Donations can be made to specific animals, which will appear in your account information.</p>
 	</div>
-	<div class="feature">
+	<div class="feature row">
 		<h2>Donations</h2>
 		<div class="feature-image">
 			<img src="http://www.placekitten.com/300/149" alt="Image" width="300" height="149" />
 		</div>
 		<p>The donations feature of our website allows your charity to accept donations online through your website. Donations are paid through PayPal, and we do not take a percentage of any donations you receive.</p>
 	</div>
-	<div class="feature">
+	<div class="feature row">
 		<h2>Custom Web Pages</h2>
 		<div class="feature-image">
 			<img src="/core/images/feature_pages.png" alt="Image" width="300" height="149" />
