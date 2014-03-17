e26669dd68c80098adbb8fba215f110fdaf81f2f
ripexz <ripexz@gmail.com>
2014-03-17 20:21:09 +0000

    Details can now be edited

--- admin/details.php
+++ admin/details.php
@@ -6,6 +6,9 @@
 	}
 	require_once('../core/lib/admin.php');
 	require_once('../core/lib/db.php');
+	require_once('../core/lib/util.php');
+	require_once('../core/lib/validation.php');
+	$errors = array();
 
 	$db = new db(null);
 	$conn = $db->connect();
@@ -15,7 +18,50 @@
 	echo '<div>';
 
 	if (isset($_POST['submit'])) {
-		//do stuff
+		$email_changed = false;
+		$pass_changed = false;
+
+		$valid = array();
+		$valid["email"] = get_required_string($_POST, "email", "Your email", 255, $errors);
+		$valid["email_2"] = get_required_string($_POST, "email_2", "Your email (repeat)", 255, $errors);
+		$valid["password"] = get_optional_string($_POST, "password", "Your password", 255, $errors);
+		$valid["password_2"] = get_optional_string($_POST, "password_2", "Your password (repeat)", 255, $errors);
+
+		if ($valid["email"] != $_SESSION["admin_email"]) {
+			$email_changed = true;
+			if ($valid["email"] != $valid["email_2"]) {
+				$errors[] = "Passwords do not match.";
+			}
+		}
+
+		if ($valid["password"] != NULL) {
+			$pass_changed = true;
+			if ($valid["password"] != $valid["password_2"]) {
+				$errors[] = "Passwords do not match.";
+			}
+		}
+
+		if ($email_changed || $pass_changed) {
+			if (count($errors) == 0) {
+				$valid["enc_pass"] = encrypt($valid["password"]);
+				$safe = $db->escape_array($conn, $valid);
+
+				$sql = "UPDATE admins SET ";
+				$sql .= $email_changed ? "email = '{$safe[email]}'" : '';
+				$sql .= $pass_changed ? "password = '{$safe[enc_pass]}'" : '';
+				$sql .= " WHERE id = {$_SESSION[admin_id]}";
+
+				$result = $conn->query($sql);
+				if ($result) {
+					echo "<div class=\"alert alert-success\">Details updated successfully.</div>";
+				}
+			}
+			else {
+				foreach ($errors as $error) {
+					echo "<div class=\"alert alert-danger\"><strong>Error: </strong>{$error}</div>";
+				}
+			}
+		}
 	}
 
 	echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'">
