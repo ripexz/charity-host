1b0424c94333ea8f95f75065e229c31a7e638185
ripexz <ripexz@gmail.com>
2014-03-18 18:22:13 +0000

    Ripped out old gallery upload code

--- admin/gallery.php
+++ admin/gallery.php
@@ -16,51 +16,6 @@
 	echo '<button class="btn btn-lg btn-primary btn-top-right" data-backdrop="static" data-keyboard="false" data-toggle="modal" data-target="#uploadModal">Upload</button>';
 	echo '<div id="imagesView">';
 
-	if (isset($_POST["submit"])) {
-		$errors = array();
-		$title = get_required_string($_POST, "imagetitle", "Image title", 255, $errors);
-
-		if ($_FILES["imagefile"]["error"] !== UPLOAD_ERR_OK) {
-			$errors[] = "Image upload failed.";
-		}
-
-		if ($_FILES["imagefile"]["size"] > 2097152) {
-			$errors[] = "Image file is too big, please select an image that is under 2 MB.";
-		}
-
-		if (count($errors) == 0) {
-			$info = getimagesize($_FILES["imagefile"]["tmp_name"]);
-			if ($info === FALSE) {
-				$errors[] = "Unable to determine image type of uploaded file.";
-			}
-			else {
-				$title = $conn->real_escape_string($title);
-				$extension = pathinfo($_FILES["imagefile"]["name"], PATHINFO_EXTENSION);
-				$filename = $charity_id . "_" . mt_rand(10, 99) . "_" . time() . '.' . $extension;
-				$upload_to = "../core/uploads/" . $filename;
-
-				if (move_uploaded_file($_FILES["imagefile"]["tmp_name"], $upload_to)) {
-					//Add db entry to images:
-					$safe_url = $conn->real_escape_string($filename);
-					$result = $conn->query("INSERT INTO images (title, url) VALUES ('{$title}', '{$safe_url}')");
-					if ($result) {
-						$image_id = $conn->insert_id;
-
-						//Add db entry to charity_images:
-						$result2 = $conn->query("INSERT INTO charity_images (image_id, charity_id) VALUES ({$image_id}, {$charity_id})");
-					}
-				}
-				else {
-					$errors[] = "Image upload failed.";
-				}
-			}
-		}
-
-		foreach ($errors as $error) {
-			echo "<div class=\"alert alert-danger\"><strong>Error: </strong>{$error}</div>";
-		}
-	}
-
 	echo '<div><input data-bind="value: searchText, valueUpdate: \'afterkeydown\'" type="text" class="form-control" placeholder="Search images by title"></div>';
 	echo '<div id="gallery-images" data-bind="foreach: visibleImages">
 			<div class="gi">
