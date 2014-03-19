878cb866c51b3682f0ce734e8571474d6f0674d7
ripexz <ripexz@gmail.com>
2014-03-19 21:03:46 +0000

    Added FAQ

--- core/pages/faq.php
+++ core/pages/faq.php
@@ -4,24 +4,279 @@
 	</div>
 	<div>
 		<h1>Help</h1>
-		<h3 id="noResults">Your search did not match any results.</h3>
+		<div>
+			<h3 id="noResults">Your search did not match any results.</h3>
+		</div>
+	</div>
+	<div>
+		<h2>Registration</h2>
 		<div class="question">
-			<h2>Question 1</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h3>How do I register?</h3>
+			<p>
+				Click on the register button, which is one of the options at the top of the homepage. 
+				You will then be brought to the register page where you fill in your details and click register.
+			</p>
 		</div>
 		<div class="question">
-			<h2>Question 2</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h3>What do I need to register?</h3>
+			<p>
+				All you need is a valid email address to register. If you want to receive donations you will also need a paypal account.
+			</p>
 		</div>
 		<div class="question">
-			<h2>Question 3</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h3>Do I need a paypal account to create a website?</h3>
+			<p>
+				No, but if you want to receive doantions you will.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Do I have to give all my details?</h3>
+			<p>
+				No, you only have to give the required details such as email address, charity name, and password. the rest is optional.
+			</p>
+		</div>
+	</div>
+	<div>
+		<h2>Donations</h2>
+		<div class="question">
+			<h3>How do I accept donations?</h3>
+			<p>
+				You must setup a paypal account if you do not already have one. Then enter your paypal username in the entry field for paypal account. 
+				Donations will then be payed to this account. That is all you need to do.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Can I view my received donations?</h3>
+			<p>
+				Yes you can view your received donations by clicking on the donations button in your user panal. 
+				Some of the donations may not be confirmed. Log in to your paypal account and view your history for a more accurate view of donations received.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Can an animal receive sponsorship?</h3>
+			<p>
+				Yes an animal can receive sponsorship. Each animal you add to your sponsorship page automatically has a donate button underneath. 
+				Users can then click this button and donate to that particular animal. The animal name will then appear in your account history showing what donations were made to what animals.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I setup a paypal account?</h3>
+			<p>
+				Setting up a paypal account is easier than you may think. Please click on a link below and follow the simple guide on the paypal site to set up an account.
+				</br><a href= "https://www.paypal.com/webapps/mpp/account-setup">Setup Account</a>
+				</br><a href= "https://www.paypal.com/ie/webapps/helpcenter/article/?solutionId=1207460&topicID=11400041&m=TCI">Receive Donations</a>
+			</p>
+		</div>
+		<div class="question">
+			<h3>Does charityhost.eu take commission on donations?</h3>
+			<p>
+				No, we provide a 100% free of charge service. However, paypal does charge a very small percentage as a transaction fee. 
+				For further information on paypal charges please visit their site.
+			</p>
+		</div>
+	</div>
+	<div>
+		<h2>Create Website></h2>
+		<div class="question">
+			<h3>How do I create a new page?</h3>
+			<p>
+				You create a new page by clicking on the pages option in the user panal, and then click add new page button.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How many pages can I create?</h3>
+			<p>
+				You can create as many pages for your website as you want.		
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I name a page?</h3>
+			<p>
+				When you click on create new page, you will need to fill in the tab called name, this will be the name and the link to your page.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I change the layout of a page?</h3>
+			<p>
+				You can change the layout by clicking on the pages button, then click on edit a page, and you will be given the option of having a sidebar on the left, right or none, 
+				and you can also change the alignment here too.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I insert a picture to a page?</h3>
+			<p>
+				Click on where in the page you want to add a picture. Then click on the small button at the top that looks like a picture frame. You can then choose an image from your gallery, and it will be inserted where ever it was you clicked to add it. Any images must first be uploaded to the gallery.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I change things like format, alignment and font on a page?</h3>
+			<p>
+				When editing or creating a page, there are options at the top, similar to microsoft word, where you can change / add things like format, alignment, indents, bullet points, links, images and more.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How to can I change the colour of my site?</h3>
+			<p>
+				Click on settings in the user panal. Then click on choose colour, and slide the colour selecter to your desired colour. This will then be the general colour theme of your site.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Will a link to this page appear in my websites menu bar?</h3>
+			<p>
+				Yes a link to the new page will automatically be added to the menu bar, and the link will be the name of the page.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Do you accommodate a Lost and Found Page?</h3>
+			<p>
+				Yes we do. You can enable or disable this page by clicking on the Lost & Found option in your user panal. 
+				We also give you the option of allowing the public to place ads on this page for their lost and found animals.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Do I have to have a Lost and Found Page?</h3>
+			<p>
+				No there is no requirement making you have a Lost and Found page. 
+				It is enabled when you first set up the account but if you click on the Lost and Found tab on the left hand side of the Control Panel. 
+				Here you can then select for the page to be disabled by clicking the radio button next to disable and then clicking save settings button just next to it. 
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I use the Lost and Found page?</h3>
+			<p>
+				To use the Lost and Found page you must first click on the Lost and Found tab on the left hand side of the Control Panel. 
+				Here you are able to view all of the lost and found post that have been put on your website. You can hover over them to delete them. 
+				If you want to post a lost animal you go to your website and click on the tab at the top that says lost and found. 
+				Here you can add an entry with a description of an animal and a picture from your computers image gallery, not your websites image Gallery.			
+			</p>
+		</div>
+		<div class="question">
+			<h3>Can anyone post on my Lost and Found Page?</h3>
+			<p>
+				Initially anyone can post to your Lost and Found page if they have a lost animal. 
+				If you do not want this you can easily switch it to where you must first approve the post before it is submitted to your website. 
+				To do this you click on the Lost and Found tab on the left hand side of the Control Panel. 
+				Here you can then select for the automatic approval to be disabled by clicking the radio button next to disable and then clicking save settings button just next to it.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I delete a Lost and Found post?</h3>
+			<p>
+				To delete a Lost and Found post you must first select the Lost and Found tab on the left hand side of the Control Panel. 
+				Then once in the Lost and Found section you can hover over the post, which will give you a clickable button where you can delete the post if you want.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do users add things to my lost and found page?</h3>
+			<p>
+				They click on add entry and fill in the details of the advert.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Do you accommodate a Sponsor an Animal Page?</h3>
+			<p>
+				Yes we do. You can enable or disable this page by clicking on the Sponsored Animals option in your user panal. 
+			</p>
+		</div>
+		<div class="question">
+			<h3>Do I have to have a Sponsor an Animal Page?</h3>
+			<p>
+				No there is no requirement making you have a Sponsor an Animal page. It is enabled when you first set up the account but if you click on the Sponsor Animals tab on the left had side of the Control Panel. Here you can then select for the page to be disabled by clicking the radio button next to disable and then clicking save settings button just next to it. 
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I use the Sponsor an Animals Page?</h3>
+			<p>
+				To use the Sponsor an Animal page you first click on the tab on the left hand side of the Control Panel. 
+				Then in the top right hand corner there is a button you can click to add animal. When clicked you have the choice of giving the animal a title, 
+				which would be the name of the animal. You also give a description of the animal. 
+				Lastly you select an image from your picture gallery on your computer, not the image Gallery on the site. 
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I delete a Sponsor an Animal Post?</h3>
+			<p>
+				To delete a sponsor an animal post you click on the Sponsor Animals tab on the left hand side of the control panel. 
+				In this tab if you hover over the post it will give you a button to click where you can delete it.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Can Animals be adopted from the website?</h3>
+			<p>
+				Yes, the public can adopt an animal by contacting the details given on the page for the site administrator / charity.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I upload an Image to the Gallery?</h3>
+			<p>
+				To upload an image to your image Gallery, you first click on the Gallery tab in the control panel, then click the upload button in the top right hand corner. 
+				When that is clicked you write out the image title, and select the image from your picture gallery on your computer.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I delete an Image from the Gallery?</h3>
+			<p>
+				To delete an Image you click on the Gallery tab in the control panel. If you hover over the image with the mouse you will then be able to click on a delete button.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How do I add or change my logo?</h3>
+			<p>
+				Go into settings from the user panal and click on 'upload new' under charity logo, and upload your desired logo. 
+			</p>
+		</div>
+		<div class="question">
+			<h3>What will the url address be for my website?</h3>
+			<p>
+				The website address, or url, will be the charityhost.eu domain followed by a forward slash, and the name of your charity with no spaces, and all in lowercase letters.
+				For example if your charity is called 'Dog Charity', the link to your homepage will be www.charityhost.eu/dogcharity
+			</p>
+		</div>
+		<div class="question">
+			<h3>Will my website work on all browsers and phones?</h3>
+			<p>
+				Yes, your website is cross browser and cross platform compatible, meaning it will work just fine on any device and in any browser.
+			</p>
+		</div>
+	</div>
+	<div>
+		<h2>Account Info</h2>
+			<div class="question">
+			<h3>Can I add other administrators to my site?</h3>
+			<p>
+				Yes you can add other admistrators by clicking on the users option in your user panal, and then click on add a user. 
+				This user will then be sent an email with their login details and a password.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Can I change my password?</h3>
+			<p>
+				Yes, you can change your password at any time by clicking on the details option in the user panal.
+			</p>
+		</div>
+		<div class="question">
+			<h3>Can I change my email address?</h3>
+			<p>
+				Yes you can change your email address by clicking on the  settings option in the user panal.
+			</p>
+		</div>
+		<div class="question">
+			<h3>What happens if I forget my password?</h3>
+			<p>
+				When you try and login but have forgotten your password, you can click on the 'forgot password' option, which will email you a new password that you can login with.
+			</p>
+		</div>
+		<div class="question">
+			<h3>What happens if I forget my email?</h3>
+			<p>
+				Contact the charityhost admins (charityhosteu@gmail.com) for assisstance.
+			</p>
+		</div>
+		<div class="question">
+			<h3>How can I contact the Charity Host Administrator if I have a problem?</h3>
+			<p>
+				If you have a major problem, our email is charityhosteu@gmail.com
+			</p>
 		</div>
 	</div>
 </div>
