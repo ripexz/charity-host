9e44d6a2a215deb65d2782ede56fe6748e0e7456
ripexz <ripexz@gmail.com>
2014-03-20 14:19:58 +0000

    Page deletion, user layout fix, details session data fix

--- admin/pages.php
+++ admin/pages.php
@@ -7,14 +7,33 @@
 	require_once('../core/lib/admin.php');
 	require_once('../core/lib/db.php');
 
+	$db = new db(null);
+	$conn = $db->connect();
+	$charity_id = (int) $_SESSION["charity_id"];
 	
 	output_admin_header("Pages", $_SESSION["charity_name"], "admin");
 	echo '<a href="editor.php" class="btn btn-lg btn-primary btn-top-right">New Page</a>';
 	echo '<div>';
 
-	$db = new db(null);
-	$conn = $db->connect();
-	$charity_id = (int) $_SESSION["charity_id"];
+	if (isset($_POST['submit'])) {
+		$page_id = (int) $_POST['id'];
+		if ($page_id > 0) {
+			// validate page belongs to charity
+			$sql = "SELECT p.*
+					FROM pages p
+						JOIN charity_pages cp ON p.id = cp.page_id
+					WHERE p.id = {$page_id}
+						AND cp.charity_id = {$charity_id}";
+			$validate = $conn->query($sql);
+			if ($validate->num_rows == 1) {
+				$res1 = $conn->query("DELETE FROM pages WHERE id = {$page_id}");
+				if ($res1) {
+					$res2 = $conn->query("DELETE FROM charity_pages WHERE id = {$page_id}");
+				}
+			}
+		}
+	}
+
 	$result = $conn->query("SELECT p.* FROM pages p JOIN charity_pages cp ON cp.page_id = p.id WHERE cp.charity_id = {$charity_id} ORDER BY p.id ASC");
 
 	if ($result) {
